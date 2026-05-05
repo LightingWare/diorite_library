28d0907eac88867b146e9f41b07ebb4878ce9603
LightningWare <146706767+LightingWare@users.noreply.github.com>
2026-05-05 15:56:17 +1000

    Update loader.lua

--- ss/loader.lua
+++ ss/loader.lua
@@ -1,1 +1,10 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
+assert(ReplicatedStorage.diorite_SS_SHARED, "Shared module not found.")
+
+local Shared = ReplicatedStorage.diorite_SS_SHARED
+
+if Shared then
+    require(Shared)
+    Shared.ssinit:FireServer()
+end
